7ed297161385ad042010a98972bd8e5a300f912f
Rio <Nesta.loisy@gmail.com>
2021-11-29 11:39:14 +0100

    data prices

--- js/data.js
+++ js/data.js
@@ -6,3 +6,42 @@ const articles = [
     }
 ]
 const priceCookie = 2
+
+const Packages = [
+    {
+        title: "Standard",
+        price: 50, 
+        benefits : [
+            "Nombre de pages : 1", 
+            "Nombre de revisions : 2",
+            "Temps de livraison : 7"
+        ],
+        desc: "Le pack Standard est un pack idéal pour une gallerie, un portfolio ou encore une page de présentation. Ce pack ne contient pas beaucoup d'avantages mais est livré rapidement."
+    },
+    {
+        title: "Pro",
+        price: 100, 
+        benefits : [
+            "Nombre de pages : 2-4", 
+            "Nombre de revisions : 4",
+            "Temps de livraison : 10-15",
+            "Code source livré",
+            "Support de payment",
+            "Nom de domaine personalisé"
+        ],
+        desc: "Le pack Pro est idéal pour un portfolio, un site vitrine ou encore un site pour une association. Ce pack a pour seul desavantage un nombre limité de page mais le temps de livraison reste tout de même très court."
+    },
+    {
+        title: "Premium",
+        price: 150, 
+        benefits : [
+            "Nombre de pages : illimité", 
+            "Nombre de revisions : 5",
+            "Temps de livraison : 15-30",
+            "Code source livré",
+            "Support de payment",
+            "Nom de domaine personalisé"
+        ],
+        desc: "Le pack Premium est parfait pour tout type de site. Il est autant adapté pour un site de dropshipping que pour un portfolio développé. Ce pack peut aussi être idéal pour un site tel que pour un site de documentation."
+    }
+]
---
+++ js/prices.js
@@ -0,0 +1,101 @@
+function intiPrices() {
+    initPackages()
+    initDetailPackages()
+}
+function initPackages() {
+    section = document.getElementById("packages")
+    index = 0
+    section.innerHTML = ""
+    indexBenefit = 0
+    Packages.forEach(package => {
+        
+        containerPackage = document.createElement("div")
+        containerPackage.setAttribute("class", `package ${(package.title).toLowerCase()} js-scrollTo`)
+        containerPackage.setAttribute("id", "package" + String(index))
+        document.getElementById("packages").appendChild(containerPackage)
+        
+        titlePackage = document.createElement("p")
+        titlePackage.setAttribute("class", "titlePackage")
+        titlePackage.setAttribute("id", "titlePackage" + String(index))
+        document.getElementById("package" + String(index)).appendChild(titlePackage)
+        document.getElementById("titlePackage" + String(index)).innerText = package.title
+        
+        pricePackage = document.createElement("p")
+        pricePackage.setAttribute("class", "pricePackage")
+        pricePackage.setAttribute("id", "pricePackage" + String(index))
+        document.getElementById("package" + String(index)).appendChild(pricePackage)
+        document.getElementById("pricePackage" + String(index)).innerText = package.price
+        
+        spanPricePackage = document.createElement("span")
+        spanPricePackage.setAttribute("class", "euroSign")
+        spanPricePackage.setAttribute("id", "euroSign" + String(index))
+        document.getElementById("pricePackage" + String(index)).appendChild(spanPricePackage)
+        document.getElementById("euroSign" + String(index)).innerText = "€"
+        
+        containerBenefitPackage = document.createElement("div")
+        containerBenefitPackage.setAttribute("class", `benefits`)
+        containerBenefitPackage.setAttribute("id", "benefits" + String(index))
+        document.getElementById("package" + String(index)).appendChild(containerBenefitPackage)
+        
+        Benefits = package.benefits
+        console.log(Benefits);
+        Benefits.forEach(benefit => {
+            benefitPackage = document.createElement("p")
+            benefitPackage.setAttribute("class", "benefit")
+            benefitPackage.setAttribute("id", "benefitPackage" + String(indexBenefit))
+            document.getElementById("benefits" + String(index)).appendChild(benefitPackage)
+            document.getElementById("benefitPackage" + String(indexBenefit)).innerText = benefit
+            indexBenefit += 1
+        });
+
+        index+=1
+    });
+}
+function initDetailPackages() {
+    section = document.getElementById("moreDetails")
+    index = 0
+    section.innerHTML = ""
+    indexBenefit = 0
+    Packages.forEach(package => {
+        
+        containerPackage = document.createElement("div")
+        containerPackage.setAttribute("class", `details ${(package.title).toLowerCase()}Details`)
+        containerPackage.setAttribute("id", "details" + String(index))
+        document.getElementById("moreDetails").appendChild(containerPackage)
+        
+        titlePackageDetails = document.createElement("p")
+        titlePackageDetails.setAttribute("class", "titlePackageDetails")
+        titlePackageDetails.setAttribute("id", "titlePackageDetails" + String(index))
+        document.getElementById("details" + String(index)).appendChild(titlePackageDetails)
+        document.getElementById("titlePackageDetails" + String(index)).innerText = package.title
+        
+        descPackage = document.createElement("p")
+        descPackage.setAttribute("class", "descPackage")
+        descPackage.setAttribute("id", "descPackage" + String(index))
+        document.getElementById("details" + String(index)).appendChild(descPackage)
+        document.getElementById("descPackage" + String(index)).innerText = package.desc
+        
+        listBenefitPackage = document.createElement("ul")
+        listBenefitPackage.setAttribute("class", `detailsPackage`)
+        listBenefitPackage.setAttribute("id", "detailsPackage" + String(index))
+        document.getElementById("details" + String(index)).appendChild(listBenefitPackage)
+
+        titleListBenefitsPackage = document.createElement("li")
+        titleListBenefitsPackage.setAttribute("class", "titleDetail")
+        titleListBenefitsPackage.setAttribute("id", "titleDetail" + String(index))
+        document.getElementById("detailsPackage" + String(index)).appendChild(titleListBenefitsPackage)
+        document.getElementById("titleDetail" + String(index)).innerText = "Avantages avec ce pack :"
+        
+        Benefits = package.benefits
+        Benefits.forEach(benefit => {
+            benefitPackage = document.createElement("li")
+            benefitPackage.setAttribute("class", "detailPackage")
+            benefitPackage.setAttribute("id", "detailPackage" + String(indexBenefit))
+            document.getElementById("detailsPackage" + String(index)).appendChild(benefitPackage)
+            document.getElementById("detailPackage" + String(indexBenefit)).innerText = benefit
+            indexBenefit += 1
+        });
+
+        index+=1
+    });
+}
