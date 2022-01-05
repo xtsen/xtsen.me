1319afb9cd0576198837e94baf24bfb8b69806c7
Rio <Nesta.loisy@gmail.com>
2022-01-05 19:21:58 +0100

    change package

--- js/data.js
+++ js/data.js
@@ -55,36 +55,40 @@ const priceCookie = 2
 const Packages = [
     {
         title: "Standard",
-        price: 50, 
+        price: 150, 
         benefits : [
-            "Nombre de pages : 1", 
+            "Nombre de pages : 3", 
             "Nombre de revisions : 2",
-            "Temps de livraison : 7"
+            "Temps de livraison : 7",
+            "Certificat SSL",
         ],
         desc: "Le pack Standard est un pack idéal pour une galerie, un portfolio ou encore une page de présentation. Ce pack ne contient pas beaucoup d'avantages mais est livré rapidement."
     },
     {
         title: "Pro",
-        price: 100, 
+        price: 250, 
         benefits : [
-            "Nombre de pages : 2-4", 
+            "Nombre de pages : 3-5", 
             "Nombre de revisions : 4",
             "Temps de livraison : 10-15",
             "Code source livré",
             "Support de payment",
+            "Certificat SSL",
             "Nom de domaine personalisé"
         ],
         desc: "Le pack Pro est idéal pour un portfolio, un site vitrine ou encore un site pour une association. Ce pack a pour seul désavantage un nombre limité de pages mais le temps de livraison reste tout de même très court."
     },
     {
         title: "Premium",
-        price: 150, 
+        price: 400, 
         benefits : [
-            "Nombre de pages : illimité", 
-            "Nombre de revisions : 5",
-            "Temps de livraison : 15-30",
+            "Nombre de pages : 5 ou plus", 
+            "Nombre de revisions : 3",
+            "Temps de livraison : > 1 mois",
             "Code source livré",
             "Support de payment",
+            "Certificat SSL",
+            "Hebergement des données en France",
             "Nom de domaine personalisé"
         ],
         desc: "Le pack Premium est parfait pour tout type de site. Il est autant adapté pour un site de drop shipping que pour un portfolio développé. Ce pack peut aussi être idéal pour un site tel que pour un site de documentation."
